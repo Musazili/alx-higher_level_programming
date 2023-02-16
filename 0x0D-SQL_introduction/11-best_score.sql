358a55f9465185543b0f1dd46aac7c88614da154
Musazili <musaziliabamu1@gmail.com>
2023-02-16 13:16:33 -0800

    update

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,6 @@
 -- ists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in MySQL server
-SELECT 'name', 'score'
+-- Records are ordered by descending score
+SELECT 'score', 'name'
 FROM 'second_table'
 WHERE 'score' >= 10
 ORDER BY 'score' DESC;
