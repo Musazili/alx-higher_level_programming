35aa3b38e37905446438fbf240bfb23f5029fb4d
Musazili <musaziliabamu1@gmail.com>
2023-03-31 13:57:40 -0700

    status fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+!#/usr/bin/python3
+"""Ascript that
+- fetches  https://alx-intranet.hbtn.io/status ans uses urllib package
+"""
+
+
+if __name__ == "__main__":
+import urllib.request
+
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    body = response.read()
+    print("- type: {}".format(type(body)))
+    print("- content: {}".format(body))
+    print("- utf8 content: {}".format(body.decode('utf-8')))
