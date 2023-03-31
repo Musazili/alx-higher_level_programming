507e9e1da817549c688fc0c2d05c6a8f1eb59662
Musazili <musaziliabamu1@gmail.com>
2023-03-31 14:15:20 -0700

    my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,11 @@
 
 
 if __name__ == "__main__":
-import urllib.request
+    import urllib.request
 
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("- type: {}".format(type(body)))
-    print("- content: {}".format(body))
-    print("- utf8 content: {}".format(body.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
+        print("- type: {}".format(type(body)))
+        print("- content: {}".format(body))
+        print("- utf8 content: {}".format(body.decode('utf-8')))
